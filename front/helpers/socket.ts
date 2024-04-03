9b322ec74eb38a05956b7c36e36fa41ce5ad3fd1
Anton Cherepinets <manickmage@ukr.net>
2024-04-03 18:22:39 +0300

    qwe

--- front/helpers/socket.ts
+++ front/helpers/socket.ts
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-const socket = io('https://server-6xofi9n9m-thienlaoos-projects-2d72e3e7.vercel.app')
+const socket = io('https://server-463zw77wl-thienlaoos-projects-2d72e3e7.vercel.app')
 
 export default socket
